781663ca726eeb078645b25df366ff74c5d0f9e8
Amanda Folson <amanda@incredibl.org>
2013-11-15 20:21:22 -0500

    Add our opers php and template files.

---
+++ php/opers.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+require_once 'lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('html');
+$twig = new Twig_Environment($loader, array(
+//    'cache' => 'cache',
+));
+
+
+$pageTitle = "Home";
+$userName = $_SESSION['username'];
+$operID = $_SESSION['id'];
+$operRole = $_SESSION['admin'];
+
+//print_r($_SESSION);
+echo $twig->render('opers.html', array('pageTitle' => $pageTitle,
+										'userName' => $userName,
+										'operID' => $operID,
+										'operRole' => $operRole
+									));
+?>
+
