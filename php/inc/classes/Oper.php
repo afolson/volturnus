31dc73d8ba5091c229310edb40e1a6ed89d9fb2c
Amanda Folson <amanda@incredibl.org>
2013-11-18 21:32:49 -0500

    No, srsly.

---
+++ php/inc/classes/Oper.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('Database.php');
+
+class Oper {
+	public function __construct() {
+		$db = new Database();
+	}
+
+	public function listOpers() {
+		$stmt = $this->db->query('SELECT `id`, `username`, `flags` FROM `opers`');
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $results;
+	}
+}
+
+?>
+
+
