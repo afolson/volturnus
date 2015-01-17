5ed8f5f87fe1224b89b542bb0526e9bbc333183a
Amanda Folson <amanda@incredibl.org>
2015-01-17 15:09:05 -0800

    Seeeeeeeeeeeds

--- app/database/migrations/2015_01_13_063930_create_bans_table.php
+++ app/database/migrations/2015_01_13_063930_create_bans_table.php
@@ -21,6 +21,53 @@ class CreateBansTable extends Migration {
 			$table->string('action');
 			$table->timestamps();
 		});
+
+		DB::table('bans')->insert(
+			array(
+				array (
+					'id' => '1',
+					'type' => 'nick',
+					'mask' => '*C*h*a*n*S*e*r*v*',
+					'reason' => 'Reserved for Services',
+					'action' => 'kill'
+				),
+				array (
+					'id' => '2',
+					'type' => 'ip',
+					'mask' => '195.86.232.81',
+					'reason' => 'Delinked server',
+					'action' => 'kill'
+				),
+				array (
+					'id' => '3',
+					'type' => 'server',
+					'mask' => 'eris.berkeley.edu',
+					'reason' => 'Get out of here.',
+					'action' => 'kill'
+				),
+				array (
+					'id' => '4',
+					'type' => 'user',
+					'mask' => '*tirc@*.saturn.bbn.com',
+					'reason' => 'Idiot',
+					'action' => 'gline'
+				),
+				array (
+					'id' => '5',
+					'type' => 'realname',
+					'mask' => 'Swat Team',
+					'reason' => 'mIRKFORCE',
+					'action' => 'gline'
+				),
+				array (
+					'id' => '6',
+					'type' => 'realname',
+					'mask' => 'sub7server',
+					'reason' => 'sub7',
+					'action' => 'kill'
+				)
+			)
+		);
 	}
 
 
